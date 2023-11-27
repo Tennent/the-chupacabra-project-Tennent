c77e482bc4085b0d7231ffe0c44bfe27ff2babfa
Koterlan David <koterlandavid@gmail.com>
2023-11-27 16:27:25 +0100

    Create script for populating database

---
+++ backend/database/populate/populate.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import fs from "fs/promises";
+import { fileURLToPath } from 'url';
+import path from "path";
+import AnimalModel from '../models/AnimalModel.js';
+import QuestModel from "../models/QuestModel.js";
+
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); // gets the directory part of the file's absolute path.
+
+dotenv.config({
+  path: path.join(__dirname, '../../config/.env'),
+  override: true,
+});
+const dbUrl = process.env.DB_URL;
+
+// Populate functions
+async function populateAnimals() {
+  const animalFilePath = path.join(
+    __dirname,
+    "../data",
+    "animals.json"
+  );
+  const animals = JSON.parse(await fs.readFile(animalFilePath));
+  await AnimalModel.deleteMany({});
+  await AnimalModel.create(animals);
+}
+
+async function populateQuests() {
+  const questFilePath = path.join(
+    __dirname,
+    "../data",
+    "quests.json"
+  );
+  const quests = JSON.parse(await fs.readFile(questFilePath));
+  await QuestModel.deleteMany({});
+  await QuestModel.create(quests);
+}
+
+// Main function to connect to db and populate
+async function main() {
+  try {
+    await mongoose.connect(dbUrl);
+    // Here we can call the populate functions
+    await populateAnimals();
+    await populateQuests();
+    console.log("Database was populated succesfully. Press ctrl + C to stop the program");
+  } catch (error) {
+    console.error(error);
+  }
+}
+main();
