f32c23c42ad7c11a6a82bbc8487c6360e375545d
Tennent <zalan.marton@gmail.com>
2023-11-29 18:02:37 +0100

    fetch and display available hero

--- frontend/src/pages/HeroDashboard/HeroDashboard.jsx
+++ frontend/src/pages/HeroDashboard/HeroDashboard.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeroInteraction from '../../components/HeroInteraction/HeroInteraction';
+import './HeroDashboard.css'
 
 export default function HeroDashboard() {
+
+  const [hero, setHero] = useState(null);
+
+  useEffect(() => {
+    const getHero = async () => {
+      try {
+        const res = await fetch('/api/v1/hero')
+        setHero(await res.json());
+      } catch (err) {
+        console.error(`Error fetching hero! ${err}`);
+      }
+    }
+    getHero();
+  }, [])
+
   return (
-    <div>
-      <HeroInteraction />
-    </div>
+    hero ?
+      <div className='selected-hero-container'>
+        <HeroInteraction hero={hero[0]} />
+      </div>
+      :
+      null
   )
 }
