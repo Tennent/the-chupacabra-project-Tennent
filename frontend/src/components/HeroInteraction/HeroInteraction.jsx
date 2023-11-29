a028f28846e456f057d202ec99f94abeab8a1ec8
Tennent <zalan.marton@gmail.com>
2023-11-29 18:02:05 +0100

    create dashboard and handle basic interactions

--- frontend/src/components/HeroInteraction/HeroInteraction.jsx
+++ frontend/src/components/HeroInteraction/HeroInteraction.jsx
@@ -1,27 +1,52 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import './HeroInteraction.css'
 
-export default function HeroInteraction() {
+export default function HeroInteraction({ hero }) {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handeHeroAction = async (propertyName, statValue) => {
+        try {
+            setSearchParams({ propertyToUpdate: propertyName });
+            const res = fetch(`/api/v1/heroAction/${searchParams.get('propertyToUpdate')}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ newStat: statValue })
+            })
+            if (res.ok) {
+                console.log(`${propertyName} updated succesful!`);
+            } else {
+                console.log(`Ooops, something went wrong during ${propertyName} updating`);
+            }
+        } catch (err) {
+            console.error(`Error updating hero stats! ${err}`);
+        }
+    };
+
     return (
-        <div className='hero-details'>
-            <h1>Select hero!</h1>
-            <div className='hero-card'>
-                <img src="" alt="hero-image" />
-                <h2>Name placeholder</h2>
-                <h3>Subname placeholder</h3>
+        <div className='selected-hero-details'>
+            <div className='selected-hero-card'>
+                <img src="https://static.vecteezy.com/system/resources/previews/024/241/089/original/colorful-cavalier-king-charles-spaniel-dog-cavalier-king-charles-spaniel-portrait-dog-sticker-clip-art-dog-lover-design-ai-generated-png.png" alt="hero-image" />
+                <h2>{hero.userinput.name} {`(${hero.userinput.gender})`}</h2>
+                <h3>{hero.creature.species}</h3>
 
-                <ul>Stats:
-                    <li>LVL</li>
-                    <li>XP</li>
-                    <li>HP</li>
-                    <li>Mood</li>
-                    <li>Gold</li>
+                <ul className='hero-stats'>Stats:
+                    <li>LVL: {hero.stats.level}</li>
+                    <li>{hero.stats.xp} XP</li>
+                    <li>{hero.stats.max_hp} HP</li>
+                    <li>Mood: {hero.stats.mood}</li>
+                    <li>{hero.stats.gold} Gold</li>
                 </ul>
 
-                <ul>Actions:
-                    <li>Work</li>
-                    <li>Pet</li>
-                    <li>Train</li>
-                </ul>
+                <div className='hero-actions'>Actions:
+                    <button type='button'>Go On Quest</button>
+                    <button type='button' onClick={() => handeHeroAction('xp', 10)}>Train</button>
+                    <button type='button' onClick={() => handeHeroAction('mood', 10)}>Pet</button>
+                    <button type='button' onClick={() => handeHeroAction('current_hp', 10)}>Feed</button>
+                </div>
             </div>
         </div>
     )
