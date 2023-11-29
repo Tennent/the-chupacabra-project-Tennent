e08c2910364ebb81bb4a4b341f559ba445786330
Tennent <zalan.marton@gmail.com>
2023-11-29 15:42:53 +0100

    update creature card structures

--- frontend/src/components/HeroDetails/HeroDetails.jsx
+++ frontend/src/components/HeroDetails/HeroDetails.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './HeroDetails.css';
 
-export default function HeroDetails() {
+export default function HeroDetails({ animal, setSelectedHero }) {
+
     return (
-        <div className='hero-details'>
-            <h1 className='title'>Select hero!</h1>
-            <div className='hero-card'>
+        <div className='hero-cards'>
+            <div className='hero-card' onClick={() => setSelectedHero(animal)}>
                 <img src="https://static.vecteezy.com/system/resources/previews/024/241/089/original/colorful-cavalier-king-charles-spaniel-dog-cavalier-king-charles-spaniel-portrait-dog-sticker-clip-art-dog-lover-design-ai-generated-png.png" alt="hero-image" />
-                <h2>Name placeholder</h2>
-                <h3>Subname placeholder</h3>
+                <h2>{animal.breed.type}</h2>
+                <h3>{animal.breed.home_location}</h3>
 
                 <ul>Stats:
-                    <li>LVL</li>
-                    <li>HP</li>
-                    <li>Mood</li>
+                    <li>LVL: {animal.stats.level}</li>
+                    <li>{animal.stats.current_hp} HP</li>
+                    <li>{animal.stats.gold} Gold</li>
+                    <li>Mood: {animal.stats.mood}</li>
                 </ul>
             </div>
         </div>
