a0f27f3748d34fd194598250afc9a69faceb0089
Koterlan David <koterlandavid@gmail.com>
2023-11-29 15:52:29 +0100

    Add hero post endpoint

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import Creature from './database/models/CreatureModel.js'
 import Hero from './database/models/HeroModel.js'
 import Quests from './database/models/QuestModel.js'
 
+
 // Construct directory path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -85,6 +86,49 @@ app.get('/api/v1/quests', async (req, res) => {
 
         console.log(`Response sent!`);
         return res.status(200).json(quests)
+
+    } catch (error) {
+        console.log(`Some error occured: ${error}`);
+        return res.status(500).json({ message: "Some error occured" })
+    }
+})
+
+app.post('/api/v1/hero', async (req, res) => {
+
+    try {
+
+        const { userinput, breed } = req.body
+
+        // Checks if a hero is already exist
+        if ((await Hero.find({})).length > 0) {
+            console.log("Hero already exists");
+            return res.status(422).json({ message: "You have alredy chosen a hero" })
+        }
+
+        const heroToAdd = new Hero({
+            userinput: {
+                name: userinput.name,
+                gender: userinput.gender
+            },
+            breed: {
+                species: breed.species,
+                image: breed.image,
+                home_location: breed.home_location
+            },
+            stats: {
+                level: 1,
+                xp: 0,
+                current_hp: 100,
+                max_hp: 100,
+                gold: 0,
+                mood: 50
+            },
+            items: []
+        })
+
+        const savedHero = await heroToAdd.save()
+        console.log(`Response sent!`);
+        return res.status(201).json(savedHero)
 
     } catch (error) {
         console.log(`Some error occured: ${error}`);
