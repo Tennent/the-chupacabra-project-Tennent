4e7d10aabb983ae57085a78c29621e39b3c0c124
Tennent <zalan.marton@gmail.com>
2023-11-29 16:54:11 +0100

    rename prop

--- frontend/src/components/HeroDetails/HeroDetails.jsx
+++ frontend/src/components/HeroDetails/HeroDetails.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './HeroDetails.css';
 
-export default function HeroDetails({ animal, setSelectedHero }) {
+export default function HeroDetails({ creature, setSelectedHero }) {
 
     return (
         <div className='hero-cards'>
-            <div className='hero-card' onClick={() => setSelectedHero(animal)}>
+            <div className='hero-card' onClick={() => setSelectedHero(creature)}>
                 <img src="https://static.vecteezy.com/system/resources/previews/024/241/089/original/colorful-cavalier-king-charles-spaniel-dog-cavalier-king-charles-spaniel-portrait-dog-sticker-clip-art-dog-lover-design-ai-generated-png.png" alt="hero-image" />
-                <h2>{animal.breed.type}</h2>
-                <h3>{animal.breed.home_location}</h3>
+                <h2>{creature.creature.species}</h2>
+                <h3>{creature.creature.home_location}</h3>
 
                 <ul>Stats:
-                    <li>LVL: {animal.stats.level}</li>
-                    <li>{animal.stats.current_hp} HP</li>
-                    <li>{animal.stats.gold} Gold</li>
-                    <li>Mood: {animal.stats.mood}</li>
+                    <li>LVL: {creature.stats.level}</li>
+                    <li>{creature.stats.current_hp} HP</li>
+                    <li>{creature.stats.gold} Gold</li>
+                    <li>Mood: {creature.stats.mood}</li>
                 </ul>
             </div>
         </div>
