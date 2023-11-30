0607f229e17ef10818c305dd263c4d26cea21068
Tennent <zalan.marton@gmail.com>
2023-11-30 19:25:59 +0100

    update content

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import './Home.css';
+
 export default function Home() {
 
     return (
-        <div>
-            <h1>This is the main page</h1>
+        <div className="home-container">
+            <img src="../../src/assets/images/logo/chupacabra-project-logo.png" alt="logo" />
         </div>
     )
 }
