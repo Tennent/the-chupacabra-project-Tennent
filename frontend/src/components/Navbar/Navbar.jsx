88c190c204d5516093bea692bc643ca89dcf2e8a
Tennent <zalan.marton@gmail.com>
2023-11-30 19:24:09 +0100

    update image import

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         <nav className='navbar'>
             <div className='navbar-logo'>
                 <Link to={'/'}>
-                    <img src="https://images.rawpixel.com/image_transparent_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsb2ZmaWNlOF92ZWN0b3JfbGluZV9hcnRfb2ZfYWJzdHJhY3Rfb3JhbmdlX2ZveF9fbWluaW1hbF9mYTg4YmRhNC0yYmRlLTRjN2QtYjZjOC03YTIzZDc1ZDhjNjIucG5n.png" alt="logo" />
+                    <img src="../../src/assets/images/logo/chupacabra-project-logo.png" alt="logo" />
                 </Link>
             </div>
             <div className='navbar-links'>
