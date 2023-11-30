d3083cf8c3d0028044fa63b6ea13a6d5468aa9b6
Gabor Kalmar <gakalmar@gmail.com>
2023-11-30 19:09:50 +0100

    Images copied to public folder, hero images updated

--- frontend/src/components/HeroDetails/HeroDetails.jsx
+++ frontend/src/components/HeroDetails/HeroDetails.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './HeroDetails.css';
 
 export default function HeroDetails({ creature, setSelectedHero }) {
-
     return (
         <div className='hero-cards'>
             <div className='hero-card' onClick={() => setSelectedHero(creature)}>
-                <img src="https://static.vecteezy.com/system/resources/previews/024/241/089/original/colorful-cavalier-king-charles-spaniel-dog-cavalier-king-charles-spaniel-portrait-dog-sticker-clip-art-dog-lover-design-ai-generated-png.png" alt="hero-image" />
+                <img src={`/images/creatures/${creature.creature.species}.png`} alt="hero-image" />
                 <h2>{creature.creature.species}</h2>
                 <h3>{creature.creature.home_location}</h3>
 
