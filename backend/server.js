65bfb4cf0aa298350f470b7f698310d7bb93a436
Koterlan David <koterlandavid@gmail.com>
2023-11-30 11:17:20 +0100

    Add static middleware

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json())
+app.use(express.static('public'))
 
 app.use((req, _, next) => {
     console.log(`Request method: ${req.method}`);
