7ebc15f949a1bca5b5b8f833ef73b18d5fa096a4
Tennent <zalan.marton@gmail.com>
2023-11-30 17:15:02 +0100

    relocate function and clean up

--- frontend/src/pages/HeroDashboard/HeroDashboard.jsx
+++ frontend/src/pages/HeroDashboard/HeroDashboard.jsx
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import HeroInteraction from '../../components/HeroInteraction/HeroInteraction';
 import './HeroDashboard.css'
+import { getHero } from '../../services/fetchHero';
 
 export default function HeroDashboard() {
 
   const [hero, setHero] = useState(null);
 
   useEffect(() => {
-    const getHero = async () => {
-      try {
-        const res = await fetch('/api/v1/hero')
-        setHero(await res.json());
-      } catch (err) {
-        console.error(`Error fetching hero! ${err}`);
-      }
+    const saveHero = async () => {
+      setHero(await getHero());
     }
-    getHero();
+    saveHero();
   }, [])
 
   return (
-    hero ?
-      <div className='selected-hero-container'>
-        <HeroInteraction hero={hero[0]} />
-      </div>
-      :
-      null
+    <>
+      {hero ?
+        <div className='selected-hero-container'>
+          <HeroInteraction hero={hero[0]} setHero={setHero} />
+        </div>
+        :
+        null
+      }
+    </>
   )
 }
