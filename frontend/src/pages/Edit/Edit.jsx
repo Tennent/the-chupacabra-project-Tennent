8d96689340c11f376dfa5582a1c6d4c7b53689be
Gabor Kalmar <gakalmar@gmail.com>
2023-12-12 10:13:22 +0100

    return object changed to components, Button handler functions created

--- frontend/src/pages/Edit/Edit.jsx
+++ frontend/src/pages/Edit/Edit.jsx
@@ -1,5 +1,8 @@
+import { EditableQuests } from '../../components/EditableQuests/EditableQuests.jsx';
+import { EditableCreatures } from '../../components/EditableCreatures/EditableCreatures.jsx';
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Edit.css";
 
 export default function Edit() {
     
@@ -33,36 +36,71 @@ export default function Edit() {
 
     function handleEditCreatureBtn(event) {
         const creatureId = event.target.id;
-        navigate(`/editcreature/${creatureId}`);
+        navigate(`/editcreature/${creatureId.slice(4)}`);
     }
 
     function handleEditQuestBtn(event) {
         const questId = event.target.id;
-        navigate(`/editquest/${questId}`);
+        navigate(`/editquest/${questId.slice(4)}`);
     }
 
+    async function handleDeleteCreatureBtn(event){
+        const creatureId = event.target.id.slice(6);
+        const isConfirmed = window.confirm("Are you sure you want to delete this creature?");
+        if (isConfirmed){
+            try{
+                const response = await fetch(`/api/v1/deletecreature/${creatureId}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+                await response.json();
+                if (response.ok){
+                    alert("Item deleted successfully!");
+                    window.location.reload();
+                } else {
+                    console.log("Item couldn't be deleted!")
+                }
+            } catch (error) {
+                console.log("An error occurred while fetching", error)
+            }
+        } else {
+            console.log("Deletion cancelled.");
+        }
+    }
+
+    async function handleDeleteQuestBtn(event){
+        const questId = event.target.id.slice(6);
+        const isConfirmed = window.confirm("Are you sure you want to delete this quest?");
+        if (isConfirmed){
+            try{
+                const response = await fetch(`/api/v1/deletequest/${questId}`, {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                });
+                await response.json();
+                if (response.ok){
+                    alert("Item deleted successfully!");
+                    window.location.reload();
+                } else {
+                    console.log("Item couldn't be deleted!")
+                }
+            } catch (error) {
+                console.log("An error occurred while fetching", error)
+            }
+        } else {
+            console.log("Deletion cancelled.");
+        }
+    }
+
+    if (!creatures || !quests) {return <>Loading...</>}
     return (
         <>
-            <div>
-                <h1>Creatures:</h1>
-                {creatures.map(creature => (
-                    <div key={creature._id}>
-                        <h3>Creature: {creature.creature.species}</h3>
-                        <p>ID: {creature._id}</p>
-                        <button id={creature._id} onClick={handleEditCreatureBtn}>Edit creature</button>
-                    </div>    
-                ))}
-            </div>
-            <div>
-                <h1>Quests:</h1>
-                {quests.map(quest => (
-                    <div key={quest._id}>
-                        <h3>{quest.name}</h3>
-                        <p>Quest ID: {quest._id}</p>
-                        <button id={quest._id} onClick={handleEditQuestBtn}>Edit quest</button>
-                    </div>    
-                ))}
-            </div>
+            <EditableCreatures creatures={creatures} handleEditCreatureBtn={handleEditCreatureBtn} handleDeleteCreatureBtn={handleDeleteCreatureBtn}/>
+            <EditableQuests quests={quests} handleEditQuestBtn={handleEditQuestBtn} handleDeleteQuestBtn={handleDeleteQuestBtn} />
         </>
     );
 }
