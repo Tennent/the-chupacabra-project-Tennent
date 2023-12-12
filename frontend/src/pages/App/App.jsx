7f2b0d293199e4e15866bb7d7013074478d95ec7
Tennent <zalan.marton@gmail.com>
2023-12-12 10:27:00 +0100

    create new routes and pass on state with user info

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -7,21 +7,23 @@ import Home from '../Home/Home'
 import SelectHero from '../SelectHero/SelectHero'
 import HeroDashboard from '../HeroDashboard/HeroDashboard'
 import Login from '../Login/Login'
+import Register from '../Register/Register'
 import ErrorPage from '../ErrorPage/ErrorPage'
 import Edit from '../Edit/Edit'
 import EditCreature from '../../components/EditCreature/EditCreature'
 import EditQuest from '../../components/EditQuest/EditQuest'
 
 export default function App() {
-
+    const [user, setUser] = useState(null)
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
-                    <Route path="selecthero" element={<SelectHero />} />
-                    <Route path="herodashboard" element={<HeroDashboard />} />
-                    <Route path="login" element={<Login />} />
+                    <Route path="selecthero" element={<SelectHero user={user} />} />
+                    <Route path="herodashboard" element={<HeroDashboard user={user} />} />
+                    <Route path="login" element={<Login setUser={setUser} />} />
+                    <Route path="register" element={<Register />} />
                     <Route path="edit" element={<Edit />} />
                     <Route path="editcreature/:creatureId" element={<EditCreature />} />
                     <Route path="editquest/:questId" element={<EditQuest />} />
