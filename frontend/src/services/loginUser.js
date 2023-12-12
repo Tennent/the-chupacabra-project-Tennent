68fe5d35ac295245f55e4b9c914c0492b9ea3bb2
Tennent <zalan.marton@gmail.com>
2023-12-12 13:01:32 +0100

    create/ew-wotk user login function

---
+++ frontend/src/services/loginUser.js
@@ -0,0 +1,19 @@
+export default async function loginUser(messageBody) {
+    try {
+        const res = await fetch('/api/v1/loginUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(messageBody)
+        })
+        if (!res.ok) {
+            alert('Invalid login info!')
+        } else {
+            alert('User logged in successfully!')
+        }
+        return res.json();
+    } catch (error) {
+        console.error(`Error durring login ${error}`);
+    }
+};
