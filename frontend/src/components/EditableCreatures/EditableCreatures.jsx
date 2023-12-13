76d98f3aaada5ad5c337ec82a4896973dddf84bf
priusz <varro.timi@gmail.com>
2023-12-13 15:55:30 +0100

    modify creature.creature

--- frontend/src/components/EditableCreatures/EditableCreatures.jsx
+++ frontend/src/components/EditableCreatures/EditableCreatures.jsx
@@ -4,7 +4,7 @@ export function EditableCreatures({ creatures, handleEditCreatureBtn, handleDele
     return <div className="container">
         <h1>Creatures:</h1>
         {creatures.map(creature => <div className="container" key={creature._id}>
-            <h2>Creature: {creature.creature.species}</h2>
+            <h2>Creature: {creature.species}</h2>
             <p>ID: {creature._id}</p>
             <div>
                 <button id={"edit"+creature._id} onClick={handleEditCreatureBtn}>Edit</button>
