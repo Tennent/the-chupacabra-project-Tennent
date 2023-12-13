f0b966295d872100ec996011209dc873caaced8b
Tennent <zalan.marton@gmail.com>
2023-12-13 09:33:39 +0100

    add alternative display when logged in

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import './Register.css';
 import postUser from '../../services/postUser';
-export default function Register() {
+export default function Register({ setUser, user }) {
+
+    //Feature hooks
+    const navigate = useNavigate();
+
     //States
     const [userName, setUserName] = useState("");
     const [userPassword, setUserPassword] = useState("");
@@ -16,15 +21,27 @@ export default function Register() {
         console.log(data);
     };
 
+    async function handleLogout() {
+        setUser(null);
+        navigate('/');
+    };
+
     return (
-        <div className='register-container'>
-            <form onSubmit={handleRegister}>
-                <label htmlFor="userName">Username</label>
-                <input id="userName" name="userName" type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
-                <label htmlFor="userPassword">Password</label>
-                <input id="userPassword" name="userPassword" type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
-                <button type="submit">Register</button>
-            </form>
-        </div>
+        <>{!user || user.message === 'Some error occured' ?
+            <div className='register-container'>
+                <form onSubmit={handleRegister}>
+                    <label htmlFor="userName">Username</label>
+                    <input id="userName" name="userName" type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
+                    <label htmlFor="userPassword">Password</label>
+                    <input id="userPassword" name="userPassword" type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} />
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+            :
+            <div className='login-container'>
+                <h1>You are already logged in!</h1>
+                <button onClick={() => handleLogout()}>Log Out</button>
+            </div>}
+        </>
     )
 }
